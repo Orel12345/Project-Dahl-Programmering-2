20b2cf5d20b4fe2273e68bce1df4806117b38efe
Orel12345 <manuelorel.nzeyimana@elev.ga.lbs.se>
2024-09-27 11:29:46 +0200

    Add project files.

---
+++ Project Dahl Programmering 2/VehicleInfo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Dahl_Programmering_2 {
+	internal class VehicleInfo {
+		public string BodyType;
+		public string FuelType;
+		public string Transmission;
+
+		public VehicleInfo(string inputBodyType, string inputFuelType, string inputTransmission) {
+			BodyType = inputBodyType;
+			FuelType = inputFuelType;
+			Transmission = inputTransmission;
+
+		}
+
+		public void SkipToCarInfo() {
+			string inputTransmission = Console.ReadLine();
+			if (inputTransmission == "El") {
+
+			}
+		}
+
+
+
+		public void PrintVehicleInfo() {
+            Console.WriteLine("Your car body type: " + BodyType + " FuelType: " + " Transmission: " + Transmission);
+        }
+	}
+}
